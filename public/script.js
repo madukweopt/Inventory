9f2344dcec067707f863d79a3284d49ee5a49bdb
Pascal <madukwepascal@gmail.com>
2026-07-22 09:31:30 +0100

    completed population of data from database to pages

--- public/script.js
+++ public/script.js
@@ -9,6 +9,8 @@ const secondBtn = document.querySelector('.second');
 const salesForm = document.querySelector('#sales-form');
 const purchaseForm = document.querySelector('#purchases-form');
 const inventoryForm = document.querySelector('#inventory-form');
+const productionForm = document.querySelector('#production-form');
+const expensesForm = document.querySelector('#expenses-form');
 
 function displaySubmenuBtn(event) {
   event.stopPropagation();
@@ -52,7 +54,6 @@ if (secondBtn) {
 function displaySelectedForm() {
   const forms = document.querySelectorAll('.card');
   const formId = window.location.hash;
-  console.log(formId);
   forms.forEach((form) => {
     form.classList.remove('show')
   });
@@ -64,6 +65,17 @@ function displaySelectedForm() {
     return;
   }
   selectedFormId.closest('.card').classList.add('show');
+  if (formId === '#sales-data') {
+    loadSalesData();
+  }else if (formId === '#inventory-data'){
+    loadInventoryData();
+  }else if (formId === '#purchases-data') {
+    loadPurchasesData();
+  }else if (formId === '#production-data') {
+    loadProductionData();
+  }else if (formId === '#expenses-data') {
+    loadExpensesData();
+  }
 }
 displaySelectedForm();
 
@@ -234,4 +246,331 @@ async function saveInventory(event) {
 if (inventoryForm) {
   inventoryForm.addEventListener('submit', saveInventory);
 }
+
+async function saveProduction(event){
+  event.preventDefault();
+  const production = {
+    production_date: document.querySelector('#production-date').value,
+    kernel_used: document.querySelector('#kernel-used').value,
+    pko_produced: document.querySelector('#oil-produced').value,
+    pkc_produced: document.querySelector('#pkc-produced').value,
+    production_hours: document.querySelector('#production-hours').value,
+    down_time: document.querySelector('#down-time').value
+  };
+  try {
+  const response = await fetch('/api/production', {
+    method: 'POST',
+    headers: { 'content-type': 'application/json'
+    },
+    body: JSON.stringify(production)
+  });
+  if (!response.ok) {
+    const problem = await response.json();
+    if (response.status === 400) {
+      alert('Failed to submit.' + problem.err);
+    }else{
+      alert('Failed to submit. Internal server problem');
+    } return;
+    }
+    const savedProduction = await response.json();
+    console.log('saved:', savedProduction);
+    alert('Production submitted successfully');
+    productionForm.reset();
+  }catch (err){
+    console.error(err);
+    alert('Network problem. Failed to submit');
+  }
+}
+if (productionForm) {
+  productionForm.addEventListener('submit', saveProduction);
+}
+
+async function saveExpenses(event) {
+  event.preventDefault();
+  const expense = {
+    expense_date: document.querySelector('#expense-date').value,
+    category: document.querySelector('#expense-category').value,
+    description: document.querySelector('#expense-description').value,
+    amount_spent: document.querySelector('#expense-amount').value
+  };
+try {
+  const response = await fetch('/api/expenses', {
+    method: 'POST',
+    headers: { 'content-type': 'application/json'
+    },
+    body: JSON.stringify(expense)
+  });
+  if (!response.ok) {
+    if (response.status === 400) {
+      const problem = await response.json();
+      alert('Failed to submit ' + problem.err);
+      console.log(problem.err);
+    }else {
+      alert('Failed to submit. Internal server error');
+    }
+    return;
+  }
+  const savedExpense = await response.json();
+  alert('Expense submitted successfully');
+  expensesForm.reset();
+  console.log('submitted:', savedExpense);
+} catch (err) {
+  console.error(err);
+  alert('Network problem. Failed to submit');
+}
+}
+if (expensesForm) {
+  expensesForm.addEventListener('submit', saveExpenses);
+}
+
+async function loadSalesData(){
+  try{
+    
+    const response = await fetch('/api/sales');
+    const tableBody = document.querySelector('#sales-data-body');
+    if (!tableBody) {
+      return;
+    }
+    if (!response.ok) {
+    const problem = await response.json();
+    alert(problem.err);
+    return;
+    }
+    const sales = await response.json();
+    tableBody.innerHTML = ``;
+    sales.forEach(function(sale){
+      const row = document.createElement('tr');
+      const submitted = new Date(sale.created_at);
+      const formatedDate = submitted.toLocaleDateString('en-NG', {
+        day: '2-digit',
+        month: '2-digit',
+        year: '2-digit'
+      }).replace(/\//g, '-');
+      const formatedTime = submitted.toLocaleTimeString('en-NG', {
+        hour: '2-digit',
+        minute: '2-digit'
+      });
+      const dateAndTime = formatedDate + ' ' + formatedTime;
+      const formatedTotal = Number(sale.total).toLocaleString('en-NG');
+        const cells = [
+          dateAndTime,
+          sale.customer,
+          sale.name,
+          sale.quantity,
+          sale.unit_price,
+          formatedTotal];
+          cells.forEach((value) => {
+            const tableData = document.createElement('td');
+            tableData.textContent = value;
+            row.appendChild(tableData);
+          });
+          tableBody.appendChild(row);
+    })
+  }
+  catch (err){
+    console.error(err);
+    alert('Failed to load Sales. Network problem');
+  }
+}
+loadSalesData();
+
+async function loadInventoryData() {
+  try {
+    const response = await fetch('/api/stock_counts');
+    if (!response.ok) {
+      const problem = await response.json();
+      console.log(problem.err);
+      alert(problem.err);
+      return;
+    }
+    const datalists = await response.json();
+    const inventoryDataBody = document.querySelector('#inventory-data-body');
+    inventoryDataBody.innerHTML ="";
+    datalists.forEach((dataItem) => {
+      const row = document.createElement('tr');
+      const submittedAt = new Date(dataItem.created_at);
+      const formatedDate = submittedAt.toLocaleDateString('en-NG', {
+        day: '2-digit',
+        month: '2-digit',
+        year: '2-digit'
+      }).replace(/\//g, '-');
+      const formatedTime = submittedAt.toLocaleTimeString('en-NG', {
+        hour: '2-digit',
+        minute: '2-digit'
+      });
+      const dateAndTime = formatedDate + ' ' + formatedTime;
+      
+      const cells = [
+        dateAndTime,
+        dataItem.name,
+        dataItem.opening_stock,
+        dataItem.received,
+        dataItem.issued,
+        dataItem.closing_stock
+        ]
+        cells.forEach((item) => {
+          const td = document.createElement('td');
+          td.textContent = item;
+          row.appendChild(td);
+        });
+        inventoryDataBody.appendChild(row);
+    });
+  } catch (error) {
+    console.error(error.message);
+    alert(error.message);
+  }
+}
+
+async function loadPurchasesData(){
+  try{
+    const purchasesDataBody = document.querySelector('#purchases-data-body');
+    const response = await fetch('/api/purchases');
+    if (response.status === 500) {
+      const problem = await response.json();
+      alert(problem.err);
+      return;
+    }
+      if (!response.ok) {
+        alert('Request failed' + ' ' + response.statusText);
+        return;
+      }
+      const purchasesList = await response.json();
+      purchasesDataBody.innerHTML = '';
+      purchasesList.forEach(purchase => {
+        const row = document.createElement('tr');
+        const submittedAt = new Date(purchase.created_at);
+        const formatedDate = submittedAt.toLocaleDateString('en-NG', {
+          day: '2-digit',
+          month: '2-digit',
+          year: '2-digit'
+        }).replace(/\//g, '-');
+        const formatedTime = submittedAt.toLocaleTimeString('en-NG', {
+          hour: '2-digit',
+          minute: '2-digit'
+        });
+        const dateAndTime = formatedDate + ' ' + formatedTime;
+        const cells = [
+          dateAndTime,
+          purchase.supplier,
+          purchase.name,
+          purchase.quantity_supplied,
+          purchase.unit_cost,
+          purchase.total_cost
+          ];
+          
+          cells.forEach((value) => {
+            const td = document.createElement('td');
+            td.textContent = value;
+            row.appendChild(td);
+          });
+          purchasesDataBody.appendChild(row)
+      });
+    } catch (error){
+      console.error(error.message)
+      alert('Something went Wrong' + ' ' + error.message);
+    };
+  };
+  
+async function loadProductionData(){
+  try{
+    const productionDataBody = document.querySelector('#production-data-body');
+    const response = await fetch('/api/production');
+    if (response.status === 500) {
+      const problem = await response.json();
+      alert(problem.err);
+      return;
+    }
+      if (!response.ok) {
+        alert('Request failed' + ' ' + response.statusText);
+        return;
+      }
+      const productionList = await response.json();
+      productionDataBody.innerHTML = '';
+      productionList.forEach(production => {
+        const row = document.createElement('tr');
+        const submittedAt = new Date(production.created_at);
+        const formatedDate = submittedAt.toLocaleDateString('en-NG', {
+          day: '2-digit',
+          month: '2-digit',
+          year: '2-digit'
+        }).replace(/\//g, '-');
+        const formatedTime = submittedAt.toLocaleTimeString('en-NG', {
+          hour: '2-digit',
+          minute: '2-digit'
+        });
+        const dateAndTime = formatedDate + ' ' + formatedTime;
+        const cells = [
+          dateAndTime,
+          production.kernel_used,
+          production.pko_produced,
+          production.pkc_produced,
+          production.pko_yield,
+          production.pkc_yield,
+          production.production_hours,
+          production.down_time
+          ];
+          
+          cells.forEach((value) => {
+            const td = document.createElement('td');
+            td.textContent = value;
+            row.appendChild(td);
+          });
+          productionDataBody.appendChild(row)
+      });
+    } catch (error){
+      console.error(error.message)
+      alert('Something went Wrong' + ' ' + error.message);
+    };
+  };
+  
+  async function loadExpensesData(){
+  try{
+    const expensesDataBody = document.querySelector('#expenses-data-body');
+    const response = await fetch('/api/expenses');
+    if (response.status === 500) {
+      const problem = await response.json();
+      alert(problem.err);
+      return;
+    }
+      if (!response.ok) {
+        alert('Request failed' + ' ' + response.statusText);
+        return;
+      }
+      const expensesList = await response.json();
+      expensesDataBody.innerHTML = '';
+      expensesList.forEach(expense => {
+        const row = document.createElement('tr');
+        const submittedAt = new Date(expense.created_at);
+        const formatedDate = submittedAt.toLocaleDateString('en-NG', {
+          day: '2-digit',
+          month: '2-digit',
+          year: '2-digit'
+        }).replace(/\//g, '-');
+        const formatedTime = submittedAt.toLocaleTimeString('en-NG', {
+          hour: '2-digit',
+          minute: '2-digit'
+        });
+        const dateAndTime = formatedDate + ' ' + formatedTime;
+        const cells = [
+          dateAndTime,
+          expense.category,
+          expense.description,
+          expense.amount_spent,
+          ];
+          
+          cells.forEach((value) => {
+            const td = document.createElement('td');
+            td.textContent = value;
+            row.appendChild(td);
+          });
+          expensesDataBody.appendChild(row)
+      });
+    } catch (error){
+      console.error(error.message)
+      alert('Something went Wrong' + ' ' + error.message);
+    };
+  };
+  
+  
+
 
--- server.js
+++ server.js
@@ -25,6 +25,65 @@ app.get('/api/items', function (req, res) {
   });
 });
 
+app.get('/api/sales', function(req, res){
+  pool.query('SELECT sales.created_at, sales.customer, items.name, sales.quantity, sales.unit_price, sales.quantity * sales.unit_price AS Total FROM sales INNER JOIN items ON sales.item_id = items.item_id ORDER BY sales.created_at DESC', function(err, result){
+    if (err) {
+      return res.status(500).json({ err: 'Could not load sales. Database connection failed' });
+    }
+    res.status(200).json(result.rows);
+  });
+});
+
+app.get('/api/stock_counts', function(req, res){
+  pool.query('SELECT items.name, stock_counts.count_date,  stock_counts.created_at,  stock_counts.received,    stock_counts.issued,               SUM(stock_counts.received - stock_counts.issued)               OVER (                       PARTITION BY stock_counts.item_id ORDER BY                  stock_counts.created_at) - (stock_counts.received - stock_counts.issued)                            AS opening_stock,                   SUM(                       stock_counts.received - stock_counts.issued)                           OVER ( PARTITION BY stock_counts.item_id ORDER BY stock_counts.created_at) AS closing_stock FROM stock_counts INNER JOIN items ON stock_counts.item_id = items.item_id ORDER BY stock_counts.created_at', function(err, result){
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ 
+        err: 'Failed to retrieve stock record.'
+      }
+      );
+    } res.status(200).json(result.rows);
+  })
+})
+
+app.get('/api/purchases', function(req, res) {
+  pool.query(
+    'SELECT purchases.created_at, purchases.supplier, purchases.quantity_supplied, items.name, purchases.unit_cost, purchases.quantity_supplied * purchases.unit_cost AS total_cost FROM purchases INNER JOIN items ON purchases.item_id = items.item_id ORDER BY purchases.created_at', function(err, result){
+      if (err) {
+        console.log(err.message);
+        return res.status(500).json({
+          err: 'Failed to retrieve purchases record'
+        })
+      } res.status(200).json(result.rows)
+    });
+  });
+  
+  app.get('/api/production', function(req, res){
+    pool.query('SELECT production.created_at, production.kernel_used, production.pko_produced, production.pkc_produced, ROUND((production.pko_produced / NULLIF(production.kernel_used, 0) * 100)::numeric, 2) AS pko_yield, ROUND((production.pkc_produced / NULLIF(production.kernel_used, 0) * 100)::numeric, 2) AS pkc_yield, production.production_hours, production.down_time FROM production ORDER BY production.created_at DESC;', function(err, result){
+      if (err) {
+        console.log(err)
+        return res.status(500).json({
+          err: 'Failed to load production record from database'
+        });
+      } res.status(200).json(result.rows);
+    })
+  })
+  
+  app.get('/api/expenses', function(req, res){
+    pool.query(`SELECT expenses.created_at, expenses.category, expenses.description,
+      expenses.amount_spent
+      FROM expenses 
+      ORDER BY 
+      expenses.created_at DESC;`, function(err, result){
+      if (err) {
+        console.log(err)
+        return res.status(500).json({
+          err: 'Failed to load expenses record from database'
+        });
+      } res.status(200).json(result.rows);
+    })
+  })
+
 app.post('/api/sales', function(req, res){
   const sales = req.body;
   if (!sales.item_id || !sales.quantity || sales.quantity <= 0 || !sales.unit_price || sales.unit_price <= 0){
@@ -67,6 +126,38 @@ app.post('/api/stock_counts', function(req, res){
     if (err) {
       console.log(err);
       return res.status(500).json({err: 'Server encountered a problem saving purchase'})
+    }
+    res.status(201).json(result.rows[0]);
+  });
+});
+
+app.post('/api/production', function(req, res){
+  const production = req.body;
+  if (!production.production_date || !production.kernel_used || production.kernel_used <= 0 || !production.pko_produced || production.pko_produced <= 0 || !production.pkc_produced || production.pkc_produced <= 0) {
+    return res.status(400).json({ err: 'Check your inputs and try again' });
+  }
+  pool.query('INSERT INTO production ( production_date, kernel_used, pko_produced, pkc_produced, production_hours, down_time ) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+  [production.production_date, production.kernel_used, production.pko_produced, production.pkc_produced, production.production_hours, production.down_time], 
+  function(err, result){
+    if (err) {
+      console.log(err)
+     return res.status(500).json({ err: 'Failed to submit. Internal server error' })
+    }
+    res.status(201).json(result.rows[0]);
+  });
+});
+
+app.post('/api/expenses', function(req, res){
+  const expense = req.body;
+  if (!expense.expense_date || !expense.category || (expense.category !== 'Expenses' && expense.category !== 'Asset/Capital Expenditure') || !expense.description || expense.description.length > 100 || !expense.amount_spent || expense.amount_spent <=0) {
+    return res.status(400).json({ err: 'Check your inputs and try again'})
+  }
+  pool.query('INSERT INTO expenses(expense_date, category, description, amount_spent) VALUES ($1, $2, $3, $4) RETURNING *',
+  [expense.expense_date, expense.category, expense.description, expense.amount_spent],
+  function(err, result){
+    if (err) {
+      console.log(err);
+      return res.status(500).json({err: 'Could not submit expense. Internal server error'})
     }
     res.status(201).json(result.rows[0]);
   })
